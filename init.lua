921a1b792e5296f7b485f7093e5163a2b75bf325
lqlklu <lqlklu@qq.com>
2023-03-14 17:14:31 +0800

    init

---
+++ init.lua
@@ -0,0 +1,10 @@
+vim.o.relativenumber = true
+vim.g.mapleader = ' '
+
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
